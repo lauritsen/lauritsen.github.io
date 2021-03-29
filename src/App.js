1fc228da5de262ec5f92684adca63f6a459df9e7
Henrik Lauritsen <lauritsen@gmail.com>
2021-03-29 22:25:18 +0200

    Basic react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Henrik Lauritsen</h1>
+      <h2>Software developer, musician & craft beer enthusiast</h2>
+    </div>
+  );
+}
+
+export default App;
